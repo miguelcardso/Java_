5c58cb4c4a09725b2769ef02721cdc9c373d323b
Miguel <miguelcardsso@gmail.com>
2021-09-03 14:26:21 -0300

    First Commit

---
+++ src/br/com/dio/PrimeiroProgramaJava.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+import br.com.dio.model.Gato;
+
+public class PrimeiroProgramaJava {
+    public static void main(String[] args) {
+        Gato gato = new Gato("Azul","Preto",18);
+        System.out.println(gato);
+
+
+
+
+        /* int a = 5;
+        int b = 6;
+        System.out.println("Hello World ="+(a+b));*/
+    }
+
+
+}
